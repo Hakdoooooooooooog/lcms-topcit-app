5f423ea76bcc0dba8fe9be98580b562561dfbb66
Darenz Jasper Hicap <hicap.darenzjasper@gmail.com>
2024-10-27 15:55:22 +0800

    Refactor API endpoints in chapter.ts and topics.ts

--- src/api/Admin/topics.ts
+++ src/api/Admin/topics.ts
@@ -2,7 +2,7 @@ import { topicInstance } from "../../lib/helpers/axios";
 
 export const createTopic = async (data: FormData) => {
   return await topicInstance
-    .post("/topic/create", data, {
+    .post("/admin/topic/create", data, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
@@ -16,7 +16,7 @@ export const editTopic = async (
   topicId: string
 ) => {
   return await topicInstance
-    .put(`/topic/update/${topicId}`, data)
+    .put(`/admin/topic/update/${topicId}`, data)
     .then((res) => res.data)
     .catch((err) => err.response.data);
 };
