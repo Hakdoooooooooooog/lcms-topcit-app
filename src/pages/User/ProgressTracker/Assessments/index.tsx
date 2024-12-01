ba0eb7ae4d0d1dbad81120bbeed62f492db3c335
Darenz Jasper Hicap <hicap.darenzjasper@gmail.com>
2024-12-01 20:25:19 +0800

    Refactor: Replace CardItems with a placeholder layout and update styling for Assessments page

--- src/pages/User/ProgressTracker/Assessments/index.tsx
+++ src/pages/User/ProgressTracker/Assessments/index.tsx
@@ -1,10 +1,24 @@
-import { CardItems } from "../../../../components/ui/PaginatedItems/CardItems";
+import { Box, Typography } from '@mui/material';
 
 const Assessments = () => {
   return (
-    <>
-      <CardItems />
-    </>
+    <Box
+      component={'div'}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+      }}
+    >
+      <Typography variant="h1">Assessments</Typography>
+
+      <Typography variant="body1">
+        This page is under construction. Please check back later.
+      </Typography>
+    </Box>
   );
 };
 
