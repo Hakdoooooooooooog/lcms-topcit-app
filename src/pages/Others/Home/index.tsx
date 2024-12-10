dec8f3141e90a04d1f1d19036664cb747da1d898
Darenz Jasper Hicap <hicap.darenzjasper@gmail.com>
2024-12-10 18:33:13 +0800

    Feat: Refactor Home component layout and enhance button functionality

--- src/pages/Others/Home/index.tsx
+++ src/pages/Others/Home/index.tsx
@@ -1,8 +1,9 @@
-import { Box, Button, Container } from "@mui/material";
+import { Box, Button, Container } from '@mui/material';
 
-import styles from "./home.module.css";
-import { Link } from "react-router-dom";
-import { useUserStore } from "../../../lib/store";
+import styles from './home.module.css';
+import { Link } from 'react-router-dom';
+import { useUserStore } from '../../../lib/store';
+import { ArrowForward } from '@mui/icons-material';
 
 const Home = () => {
   const { user } = useUserStore((state) => ({
@@ -11,7 +12,7 @@ const Home = () => {
 
   return (
     <Container
-      component={"section"}
+      component={'section'}
       classes={{
         root: styles.hero_section,
       }}
@@ -21,27 +22,57 @@ const Home = () => {
         <div className={styles.hero_content_left}>
           <h1 className={styles.hero_title}>Welcome, {user.username}</h1>
           <p className={styles.hero_description}>
-            This is your one-stop online learning content management system in preparing for your
-            TOPCIT examination.
+            This is your one-stop online learning content management system in
+            preparing for your TOPCIT examination.
           </p>
 
-          <Link to="/about" className={styles.hero_link}>
-            <Button
-              variant="contained"
+          <Button
+            component={Link}
+            to="/learning-hub"
+            variant="contained"
+            sx={{
+              backgroundColor: 'green',
+              color: '#fff',
+              '&:hover': {
+                backgroundColor: '#388E3C',
+              },
+              padding: '10px 20px',
+            }}
+            className={styles.hero_link}
+          >
+            Start Learning{' '}
+            <ArrowForward
               sx={{
-                backgroundColor: "green",
-                color: "#fff",
-                "&:hover": {
-                  backgroundColor: "#388E3C",
-                },
-                padding: "10px 20px",
+                marginLeft: '5px',
+                fontSize: '1.2rem',
               }}
-            >
-              Get Started
-            </Button>
-          </Link>
+            />
+          </Button>
+
+          <Button
+            component={Link}
+            to="/about"
+            variant="contained"
+            sx={{
+              backgroundColor: 'green',
+              color: '#fff',
+              '&:hover': {
+                backgroundColor: '#388E3C',
+              },
+              padding: '10px 20px',
+            }}
+            className={styles.hero_link}
+          >
+            Learn more about TOPCIT{' '}
+            <ArrowForward
+              sx={{
+                marginLeft: '5px',
+                fontSize: '1.2rem',
+              }}
+            />
+          </Button>
         </div>
-        <Box component={"div"} className="flex items-center justify-center">
+        <Box component={'div'} className="flex items-center justify-center">
           <img src="hero-img.webp" alt="Hero" className="h-auto max-w-full" />
         </Box>
       </div>
